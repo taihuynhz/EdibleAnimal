4f18e82dc464aa3b61ec3051e1cc7ad6f4fc71d7
taihuynhz <129383417+taihuynhz@users.noreply.github.com>
2023-04-23 14:39:51 +0700

    Edible Animal

---
+++ Chicken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAnimalAndInterfaceEdible
+{
+    public class Chicken : Animal, Edible
+    {
+        public override string MakeSound()
+        {
+            return "Chicken: cluck-cluck!";
+        }
+        public string HowToEat()
+        {
+            return "could be fried";
+        }
+    }
+}
---
+++ Orange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAnimalAndInterfaceEdible
+{
+    public class Orange : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Orange could be juiced";
+        }
+    }
+}
